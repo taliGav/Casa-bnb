3a05d422b7a4dbeaebea9fd6ce552a119cdcc90e
Tali <taligavrielov@gmail.com>
2022-03-23 01:15:35 +0200

    preview

--- frontend/src/components/stay-list.vue
+++ frontend/src/components/stay-list.vue
@@ -1,15 +1,20 @@
 <template>
-  <section class="list-container">
+  <section>
     <button @click="$router.push('/stay/edit')" class="btn btn-secondary">
       add a new stay
     </button>
-    <ul class="stay-list py-1 clean-list grid gap-2">
-      <li v-for="stay in stays" :key="stay._id"
-      class="stay-preview-container p-2 flex flex-col gap-1"
-      @click="$router.push(`/stay/${stay._id}`)">
-        <stay-preview :stay="stay" />
-      </li>
-    </ul>
+    <div class="list-container flex flex-wrap items-center gap-1">
+      <ul class="stay-list py-1 clean-list grid gap-2">
+        <li
+          v-for="stay in stays"
+          :key="stay._id"
+          class="stay-preview-container p-2 flex flex-col gap-1"
+          @click="$router.push(`/stay/${stay._id}`)"
+        >
+          <stay-preview :stay="stay" />
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
--- frontend/src/components/stay-preview.vue
+++ frontend/src/components/stay-preview.vue
@@ -1,30 +1,68 @@
 <template>
   <section class="stay-preview">
-          <div class="flex items-center justify-between gap-2">
-            <h3 class="uppercase">{{ stay.name }}</h3>
-            <p class="clr-teal fw-bold">${{ stay.price }}</p>
-          </div>
-          <div class="labels flex flex-wrap items-center gap-1">
-            <!-- <custom-label
+    <article class="preview-card">
+      <div class="stay-imgs-container">
+        <preview-carousel> </preview-carousel>
+      </div>
+      <div class="flex items-center justify-between gap-2">
+        <!-- <p>{{ stay.type }} in {{ stay.loc.country }}</p> -->
+        <!-- <p>{{stay.type}} in {{stay.loc.country}}</p> -->
+        <h3 class="uppercase">{{ stay.name }}</h3>
+
+        <div class="rating">⭐{{ avgRating }} ({{ reviewsCount }} reviews)</div>
+
+        <p class="clr-teal fw-bold">${{ stay.price }} /NIGHT</p>
+      </div>
+    </article>
+    <div class="labels flex flex-wrap items-center gap-1">
+      <!-- <custom-label
             v-for="label in stay.labels"
             :key="label"
             :label="label"
           /> -->
-          </div>
+    </div>
     <!-- <slot></slot> -->
   </section>
 </template>
 
 <script>
-// import customLabel from './custom-label.vue'
+import PreviewCarousel from "./preview-carousel.vue";
 
 export default {
-  name: 'stay-preview',
+  name: "stay-preview",
   props: {
     stay: Object,
   },
+  data() {
+    return {};
+  },
+
   components: {
+    PreviewCarousel,
     // customLabel,
   },
-}
+  computed: {
+    avgRating() {
+      let ratingSum = this.stay.reviews.reduce((a,b) => (a.rate + b.rate));
+      console.log("ratingSum", ratingSum);
+      console.log("stay.reviews.length", this.stay.reviews.length);
+      let avgRating = ratingSum / this.stay.reviews.length;
+      return avgRating;
+    },
+    reviewsCount() {
+      return this.stay.reviews.length;
+    },
+  },
+};
 </script>
+
+
+// import customLabel from './custom-label.vue'
+
+// stay specifics needed on stay card/preview-
+// imgs carousle
+//  type in place
+// title : name
+// max-cap,bedrooms, bed num , bath num \\ לא מופיע בתצוגה קטנה
+// amenities: kitchen, free parking , Wifi \\ לא מופיע בתצוגה קטנה
+// avg rating (reviews count)     price + currency/night
