a21227bf20d54a3306096a260712b14f40d2a21b
Tali <taligavrielov@gmail.com>
2022-03-22 23:36:32 +0200

    list & preview structure update

--- frontend/src/components/stay-list.vue
+++ frontend/src/components/stay-list.vue
@@ -4,24 +4,11 @@
       add a new stay
     </button>
     <ul class="stay-list py-1 clean-list grid gap-2">
-      <stay-preview v-for="stay in stays" :key="stay._id" :stay="stay">
-        <li
-          @click="$router.push(`/stay/${stay._id}`)"
-          class="stay-preview p-2 flex flex-col gap-1"
-        >
-          <div class="flex items-center justify-between gap-2">
-            <h3 class="uppercase">{{ stay.name }}</h3>
-            <p class="clr-teal fw-bold">${{ stay.price }}</p>
-          </div>
-          <div class="labels flex flex-wrap items-center gap-1">
-            <!-- <custom-label
-            v-for="label in stay.labels"
-            :key="label"
-            :label="label"
-          /> -->
-          </div>
-        </li>
-      </stay-preview>
+      <li v-for="stay in stays" :key="stay._id"
+      class="stay-preview-container p-2 flex flex-col gap-1"
+      @click="$router.push(`/stay/${stay._id}`)">
+        <stay-preview :stay="stay" />
+      </li>
     </ul>
   </section>
 </template>
@@ -30,17 +17,13 @@
 import CustomLabel from "./custom-label.vue";
 import stayPreview from "./stay-preview.vue";
 
-// import CustomLabel from './custom-label.vue'
-// import stayPreview from './stay-preview.vue'
-
 export default {
   name: "stay-list",
   props: {
     stays: Array,
   },
   components: {
-    // stayPreview,
-    // CustomLabel,
+    stayPreview,
   },
 };
 </script>
--- frontend/src/components/stay-preview.vue
+++ frontend/src/components/stay-preview.vue
@@ -1,6 +1,16 @@
 <template>
-  <section>
-
+  <section class="stay-preview">
+          <div class="flex items-center justify-between gap-2">
+            <h3 class="uppercase">{{ stay.name }}</h3>
+            <p class="clr-teal fw-bold">${{ stay.price }}</p>
+          </div>
+          <div class="labels flex flex-wrap items-center gap-1">
+            <!-- <custom-label
+            v-for="label in stay.labels"
+            :key="label"
+            :label="label"
+          /> -->
+          </div>
     <!-- <slot></slot> -->
   </section>
 </template>
