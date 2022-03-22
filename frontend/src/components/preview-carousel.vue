7b284567c26734845616425323408f5d61618988
Tali <taligavrielov@gmail.com>
2022-03-22 19:48:31 +0200

    preview carousle added

---
+++ frontend/src/components/preview-carousel.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="carousel">
+    <el-carousel :interval="4000" type="card" height="300px">
+      <el-carousel-item v-for="(_, idx) in 12" :key="idx">
+        <img
+          class="img-in-card"
+          :src="'/src/assets/images/' + idx + '.png'"
+          alt=""
+        />
+      </el-carousel-item>
+    </el-carousel>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "carousel",
+};
+</script>
