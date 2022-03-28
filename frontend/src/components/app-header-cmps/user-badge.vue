0ed463749333e5f0e855660853c0e4bb470edd2e
Tali <taligavrielov@gmail.com>
2022-03-28 17:01:28 +0300

    GUEST USER MENU

--- frontend/src/components/app-header-cmps/user-badge.vue
+++ frontend/src/components/app-header-cmps/user-badge.vue
@@ -8,9 +8,24 @@
         alt=""
       />
     </div>
-    <div v-if="isOpen">
-      <p @click="openLogInModal">Log in</p>
-      <p @click="logout">Log out</p>
+    <div class="guest-user-menu" v-if="isOpen">
+      <div class="guest-user-commands">
+        <div class="sign-up-command-label command label" @click="openLogInModal">
+          <p>Sign up</p>
+        </div>
+        <div class="log-in-command-label  command label" @click="openLogInModal">
+          <p>Log in</p>
+        </div>
+        <div class="log-out-command-label  command label" @click="logout">
+          <p>Log out</p>
+        </div>
+      </div>
+      <div class="host-your-home-command-label  command label" @click="goToHostRegistration">
+        <p>Host your home</p>
+      </div>
+      <div class="help-label label" @click="goToHelp">
+        <p>Help</p>
+      </div>
     </div>
     <login-modal @closeModal="isModalOpen = false" v-if="isModalOpen" />
   </section>
@@ -18,40 +33,34 @@
 
 
 <script>
-import loginModal from './login-modal.vue'
+import loginModal from "./login-modal.vue";
 
 export default {
-	name: 'user-badge',
-	data() {
-		return {
-			isOpen: false,
-			isModalOpen: false,
-		};
-	},
-	created() {
-	},
-	destroyed() {
-	},
-	methods: {
-		openUserBar() {
-			this.isOpen = !this.isOpen;
-		},
-		openLogInModal() {
-			this.isModalOpen = !this.isModalOpen;
-		},
-		async logout() {
-      		await this.$store.dispatch({ type: 'logout' })
-    	},
-		
-	},
-	computed: {
-	},
-	watch: {
-		
-	},
-	components: {
-		loginModal
-	},
+  name: "user-badge",
+  data() {
+    return {
+      isOpen: false,
+      isModalOpen: false,
+    };
+  },
+  created() {},
+  destroyed() {},
+  methods: {
+    openUserBar() {
+      this.isOpen = !this.isOpen;
+    },
+    openLogInModal() {
+      this.isModalOpen = !this.isModalOpen;
+    },
+    async logout() {
+      await this.$store.dispatch({ type: "logout" });
+    },
+  },
+  computed: {},
+  watch: {},
+  components: {
+    loginModal,
+  },
 };
 </script>
 <style>
--- frontend/src/components/app-header.vue
+++ frontend/src/components/app-header.vue
@@ -1,105 +1,109 @@
 <template>
-	<header
-		class="main-header main-layout"
-		ref="header"
-		:class="{ visible: !isOpen }"
-		:style="{ backgroundColor: searchStatus }"
-	>
-		<div class="header-content flex space">
-			<div class="logo flex just align just">
-				<h2>
-					<router-link :class="{ 'color-theme': colorsChange }" to="/">
-						casa<span class="casa-symbol">⌂</span
-						><span class="clr-teal">bnb</span>
-					</router-link>
-				</h2>
-			</div>
-			<small-search-bar
-				:isOpen="isOpen"
-				@openSearch="openSearch"
-			></small-search-bar>
-			<nav class="nav-bar flex just align space">
-				<div class="explore-link flex just align">
-					<router-link :class="{ 'color-black': colorsChange }" to="/stay"
-						>Explore</router-link
-					>
-				</div>
-				<div class="host-link flex just align">
-					<router-link :class="{ 'color-black': colorsChange }" to="/"
-						>Become a Host</router-link
-					>
-				</div>
-				<user-badge></user-badge>
-			</nav>
-		</div>
-		<stay-search v-if="isOpen"></stay-search>
-	</header>
+  <header
+    class="main-header main-layout"
+    ref="header"
+    :class="{ visible: !isOpen }"
+    :style="{ backgroundColor: searchStatus }"
+  >
+    <div class="header-content flex space">
+		
+      <div class="logo flex just align just">
+        <h2>
+          <router-link :class="{ 'color-theme': colorsChange }" to="/">
+            casa<span class="casa-symbol">⌂</span>
+            <span class="clr-teal">bnb</span>
+          </router-link>
+        </h2>
+      </div>
+
+      <small-search-bar
+        :isOpen="isOpen"
+        @openSearch="openSearch"
+      ></small-search-bar>
+
+      <nav class="nav-bar flex just align space">
+        <div class="explore-link flex just align">
+          <router-link :class="{ 'color-black': colorsChange }" to="/stay"
+            >Explore</router-link
+          >
+        </div>
+        <div class="host-link flex just align">
+          <router-link :class="{ 'color-black': colorsChange }" to="/"
+            >Become a Host</router-link
+          >
+        </div>
+
+        <user-badge></user-badge>
+      </nav>
+    </div>
+    <stay-search v-if="isOpen"></stay-search>
+  </header>
 </template>
 <script>
-import userBadge from './app-header-cmps/user-badge.vue';
-import staySearch from '../components/stay-search.vue';
-import datePicker from '../components/date-picker.vue';
-import smallSearchBar from '../components/small-search-bar.vue';
+import userBadge from "./app-header-cmps/user-badge.vue";
+import staySearch from "../components/stay-search.vue";
+import datePicker from "../components/date-picker.vue";
+import smallSearchBar from "../components/small-search-bar.vue";
 
 export default {
-	name: 'app-header',
-	data() {
-		return {
-			isOpen: null,
-			colorsChange: null,
-			isSearchClicked: false,
-		};
-	},
-	created() {
-		window.addEventListener('scroll', this.handleScroll);
-	},
-	destroyed() {
-		window.removeEventListener('scroll', this.handleScroll);
-	},
-	methods: {
-		handleScroll() {
-			if (window.scrollY === 0 && this.$route.path === '/') {
-				this.colorsChange = false;
-				this.isOpen = true;
-			}
-			if (window.scrollY !== 0) {
-				this.colorsChange = true;
-				this.isOpen = false;
-				this.isSearchClicked = false;
-			}
-		},
-		openSearch() {
-			console.log('header opening search');
-			this.isOpen = true;
-			this.isSearchClicked = true;
-		},
-		checkPagePath() {
-			if (this.$route.path !== '/') {
-				this.isOpen = false;
-				this.colorsChange = true;
-			} else {
-				this.isOpen = true;
-				this.colorsChange = false;
-				this.isSearchClicked = false;
-			}
-		},
-	},
+  name: "app-header",
+  data() {
+    return {
+      isOpen: null,
+      colorsChange: null,
+      isSearchClicked: false,
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY === 0 && this.$route.path === "/") {
+        this.colorsChange = false;
+        this.isOpen = true;
+      }
+      if (window.scrollY !== 0) {
+        this.colorsChange = true;
+        this.isOpen = false;
+        this.isSearchClicked = false;
+      }
+    },
+    openSearch() {
+      console.log("header opening search");
+      this.isOpen = true;
+      this.isSearchClicked = true;
+    },
+    checkPagePath() {
+      if (this.$route.path !== "/") {
+        this.isOpen = false;
+        this.colorsChange = true;
+      } else {
+        this.isOpen = true;
+        this.colorsChange = false;
+        this.isSearchClicked = false;
+      }
+    },
+  },
 
-	computed: {
-		searchStatus() {
-			if (this.isSearchClicked) return 'rgba(255, 255, 255)';
-		},
-	},
-	watch: {
-		$route(to, from) {
-			this.checkPagePath();
-		},
-	},
-	components: {
-		userBadge,
-		staySearch,
-		datePicker,
-		smallSearchBar,
-	},
+  computed: {
+    searchStatus() {
+      if (this.isSearchClicked) return "rgba(255, 255, 255)";
+    },
+  },
+  watch: {
+    $route(to, from) {
+      this.checkPagePath();
+    },
+  },
+  components: {
+    userBadge,
+    staySearch,
+    datePicker,
+    smallSearchBar,
+  },
 };
 </script>
