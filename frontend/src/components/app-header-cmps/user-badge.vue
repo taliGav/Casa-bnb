85b76ce02920cbf4bd44c332ea859fc789c8a062
Tali <taligavrielov@gmail.com>
2022-03-29 23:49:58 +0300

    badge imgs

--- frontend/src/components/app-header-cmps/user-badge.vue
+++ frontend/src/components/app-header-cmps/user-badge.vue
@@ -3,24 +3,22 @@
     <div @click="openUserBar" class="user-badge flex align just space">
       <span class="badge-hamburger">☰</span>
       <svg
+        v-if="!user"
         class="host-img"
         viewBox="0 0 32 32"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
         role="presentation"
         focusable="false"
-        style="display: block; height: 100%; width: 100%; fill: currentcolor"
+        style="display: block; height: 80%; width: 80%; fill: currentcolor"
       >
         <path
           d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"
         ></path>
       </svg>
 
-      <img
-        class="badge-image"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1rfdGOYPCcbada6QNqTauI3LH-SpptOXVrA&usqp=CAU"
-        alt=""
-      />
+      <img v-else class="badge-image" :src="userImg" alt="" />
+      <!-- src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1rfdGOYPCcbada6QNqTauI3LH-SpptOXVrA&usqp=CAU" -->
     </div>
     <!-- <div class="relative"> -->
     <user-menu
@@ -44,7 +42,9 @@ export default {
       isMenuOpen: false,
     };
   },
-  created() {},
+  created() {
+    this.user = this.$store.getters.user;
+  },
   methods: {
     openUserBar() {
       this.user = this.$store.getters.user;
@@ -64,7 +64,11 @@ export default {
       // this.isMenuOpen = false;
     },
   },
-  computed: {},
+  computed: {
+    userImg() {
+      return this.user.imgUrl;
+    },
+  },
   watch: {},
   components: {
     userMenu,
