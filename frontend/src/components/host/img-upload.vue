b6bb95ecb8de064e4d570c5875bc5577add109b5
Tali <taligavrielov@gmail.com>
2022-03-29 22:44:18 +0300

    img-upload

--- frontend/src/components/host/img-upload.vue
+++ frontend/src/components/host/img-upload.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
     <label v-if="!isLoading">
-      <img src="./../assets/img-upload-func/upload.png" alt="" />
+      <img src="./../../assets/img-upload-func/upload.png" alt="" />
       <input type="file" @change="onUploadImg" hidden />
     </label>
-    <img v-else src="./../assets/img-upload-func/loader.gif" alt="" />
+    <img v-else src="./../../assets/img-upload-func/loader.gif" alt="" />
   </div>
 </template>
 
 <script>
-  import {uploadImg} from './../../services/imgUpload.service.js';
-  export default {
-    data() {
-      return {
-        isLoading: false,
-      };
+import { uploadImg } from "./../../services/imgUpload.service.js";
+export default {
+  name: "img-upload",
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    async onUploadImg(ev) {
+      this.isLoading = true;
+      const res = await uploadImg(ev);
+      this.$emit("save", res.url);
+      this.isLoading = false;
     },
-    methods: {
-      async onUploadImg(ev) {
-        this.isLoading = true;
-        const res = await uploadImg(ev);
-        this.$emit('save', res.url);
-        this.isLoading = false;
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style></style>
