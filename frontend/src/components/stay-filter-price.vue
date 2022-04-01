0d8921943b51f577c27aaf98b4f00ba8c8d76813
Igor <izingor@gmail.com>
2022-04-01 14:02:23 +0300

    updated price histogram

--- frontend/src/components/stay-filter-price.vue
+++ frontend/src/components/stay-filter-price.vue
@@ -1,78 +1,60 @@
 <template>
-  <section class="price-modal">
-    <div class="slider">
-      <HistogramSlider
-        :width="200"
-        :bar-height="100"
-        :data="prices"
-        :min="0"
-        :max="maxPrice"
-        @change="slider"
-      />
-    </div>
-    <hr />
-    <div class="price-modal-btns">
-      <button>clear</button>
-      <button @click="set">set</button>
-    </div>
-  </section>
+	<section class="price-modal">
+		<div class="slider">
+			<HistogramSlider
+				width="400"
+				:bar-height="80"
+				:data="prices"
+				:min="0"
+				:max="maxPrice"
+				@change="slider"
+				primaryColor="rgb(163, 163, 163)"
+				labelColor="rgb(151, 151, 151)"
+				barRadius="1"
+				barWidth="7"
+				forceEdges="true"
+			/>
+		</div>
+		<hr />
+		<div class="price-modal-btns">
+			<button>clear</button>
+			<button @click="set">set</button>
+		</div>
+	</section>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from 'vue';
 export default {
-  name: 'stay-filter-price',
-  props: {
-    stays: Array,
-  },
-  data() {
-    return {
-      prices: this.stays.map(stay => stay.price),
-      minPrice:0,
-      maxPrice:this.maxPrice,
-    }
-  },
-  created() {
-    this.max = this.maxPrice
-  },
-  watch: {
-    },
-  computed: {
-    maxPrice(){
-     return Math.max(...this.prices) 
-    }
-  },
-  methods: {
-    slider(event){
-      this.minPrice = event.from;
-      this.maxPrice = event.to;
-    },
-    set(){
-      this.$emit("setPrice", this.minPrice,this.maxPrice)
-    }
-  },
-}
+	name: 'stay-filter-price',
+	props: {
+		stays: Array,
+	},
+	data() {
+		return {
+			prices: this.stays.map((stay) => stay.price),
+			minPrice: 0,
+			maxPrice: this.maxPrice,
+		};
+	},
+	created() {
+		this.max = this.maxPrice;
+	},
+	watch: {},
+	computed: {
+		maxPrice() {
+			return Math.max(...this.prices);
+		},
+	},
+	methods: {
+		slider(event) {
+			this.minPrice = event.from;
+			this.maxPrice = event.to;
+		},
+		set() {
+			this.$emit('setPrice', this.minPrice, this.maxPrice);
+		},
+	},
+};
 </script>
-<style scoped>
-.slider-demo-block {
-  display: flex;
-  align-items: center;
-  width: 120px;
-}
-.slider-demo-block .el-slider {
-  margin-top: 0;
-  margin-left: 12px;
-}
-.irs-to{
-  background-color:black !important;
-}
-.irs-bar{
-  background-color:black !important;
-}
-.irs-form{
-  background-color:black !important;
-}
-
-
-
-</style>
+<style scoped></style>
