d316b09b2e1a50658ee3de2ddf94442516ae4826
Igor <izingor@gmail.com>
2022-04-03 19:52:24 +0300

    cursor pointer on stay previews

--- frontend/src/views/stay-details.vue
+++ frontend/src/views/stay-details.vue
@@ -1,24 +1,24 @@
 <template>
-  <section v-if="stay" class="details-layout stay-details">
-    <details-header :stay="stay" />
-    <details-gallery :stay="stay" />
+	<section v-if="stay" class="details-layout stay-details">
+		<details-header :stay="stay" />
+		<details-gallery :stay="stay" />
 
-    <el-carousel class="details-carousel-item" :interval="5000" arrow="always">
-      <el-carousel-item v-for="item in stay.imgUrls" :key="item">
-        <!-- <h3>{{ item }}</h3> -->
-        <img class="details-carousel-img" :src="item" alt="" />
-      </el-carousel-item>
-    </el-carousel>
+		<el-carousel class="details-carousel-item" :interval="5000" arrow="always">
+			<el-carousel-item v-for="item in stay.imgUrls" :key="item">
+				<!-- <h3>{{ item }}</h3> -->
+				<img class="details-carousel-img" :src="item" alt="" />
+			</el-carousel-item>
+		</el-carousel>
 
-    <div class="details-grid">
-      <div class="details-main-container full flex">
-        <div class="main-inner-container">
-          <details-host-main :stay="stay" />
-        </div>
-      </div>
-      <div class="checkout-container">
-        <details-checkout :stay="stay" />
-      </div>
+		<div class="details-grid">
+			<div class="details-main-container full flex">
+				<div class="main-inner-container">
+					<details-host-main :stay="stay" />
+				</div>
+			</div>
+			<div class="checkout-container">
+				<details-checkout :stay="stay" />
+			</div>
 
 			<div class="amenities-container">
 				<div class="amenities-header flex">
@@ -47,9 +47,9 @@
 		</div>
 		<details-reviews v-if="stay" :stay="stay" />
 
-    <details-map :stay="stay" />
+		<details-map :stay="stay" />
 
-    <!-- <div v-if="user?.isAdmin" class="btn-group gap-1">
+		<!-- <div v-if="user?.isAdmin" class="btn-group gap-1">
       <button
         @click="$router.push(`/stay/edit/${stay._id}`)"
         class="btn btn-primary"
@@ -59,11 +59,11 @@
       <button @click="$router.push('/stay')" class="btn btn-secondary">
         go back
       </button> -->
-    <!-- <button @click="removeStay" class="btn btn-danger">delete stay</button> -->
-    <!-- </div> -->
-  </section>
+		<!-- <button @click="removeStay" class="btn btn-danger">delete stay</button> -->
+		<!-- </div> -->
+	</section>
 
-  <!-- <p class="clr-teal fw-bold">${{ stay.price }} /NIGHT</p> -->
+	<!-- <p class="clr-teal fw-bold">${{ stay.price }} /NIGHT</p> -->
 </template>
 
 <script>
